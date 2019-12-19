1c6ef25cf12238bd186a63051fbc41b0867c4535
Brad Jasperson <brad.jasperson@imaginelearning.com>
2019-12-18 17:16:34 -0700

    Xamarin QuickStart

--- C971/C971/App.xaml.cs
+++ C971/C971/App.xaml.cs
@@ -1,17 +1,30 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using C971.Data;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace C971
 {
     public partial class App : Application
     {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
